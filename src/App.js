58b8cfd33089abe8fa3a26803172bf7c38d0d175
JP Millan <jp.millan@flybuys.com.au>
2021-01-25 15:13:52 +1100

    create simple react page with login and use jwt to consume api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import AuthService from './components/AuthService';
+import withAuth from './components/withAuth';
+const Auth = new AuthService();
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      offers: []
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+  componentDidMount() {
+    Auth.fetchOffers().then((response) => {
+      console.log('response:', response);
+      this.setState({ offers: response})
+    });
+   }
+  handleLogout(){
+    Auth.logout()
+    this.props.history.replace('/login');
+  }
+
+  render() {
+    const offers = this.state.offers;
+    console.log('offers:', offers)
+    return (
+      <div className="App">
+        <div className="card">
+            <div className="card-body">
+              <h5 className="card-title">Todays Offers for {Auth.getUser()}</h5>
+            </div>
+            <div className="offer-container">
+            {
+            offers.map((o, i) => {
+              return <div key={i} className="box">
+                <b>Offer ID: </b>{o.offerID}<br />
+                <b>Name: </b>{o.name}<br />
+                <b>Expiry: </b>{o.offerExpiry}<br />
+                <b>Details: </b>{o.details}
+              </div>
+            })}            
+            </div>
+      </div>
+        <p className="App-intro">
+          <button type="button" className="form-submit" onClick={this.handleLogout.bind(this)}>Logout</button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
-export default App;
+export default withAuth(App);
--- src/index.js
+++ src/index.js
@@ -2,16 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import registerServiceWorker from './registerServiceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+// Our Components
+import Login from './components/Login';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Router>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route exact path="/login" component={Login} />
+        </div>
+    </Router>
+    , document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+registerServiceWorker();
